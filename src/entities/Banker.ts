ff8e0401f76d68dc4e850ddf08ceb54b16cc26fd
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-08-11 16:43:36 +0500

    added a simple comment

--- src/entities/Banker.ts
+++ src/entities/Banker.ts
@@ -1,34 +1,27 @@
-import { Entity, Column, ManyToMany, JoinTable } from "typeorm"
-import { Person } from "./utils/Person"
-import { Client } from "./Client"
+import { Entity, Column, ManyToMany, JoinTable } from "typeorm";
+import { Person } from "./utils/Person";
+import { Client } from "./Client";
 
-
-@Entity('banker')
+//Add comment
+@Entity("banker")
 export class Banker extends Person {
-   
+	@Column({
+		unique: true,
+		length: 10,
+	})
+	employee_number: string;
 
-  @Column({
-    unique: true,
-    length: 10
-  })
-  employee_number: string
-
-  @ManyToMany(
-    () => Client
-  )
-
-
-  @JoinTable({
-    name: "Bankers_clients",
-    joinColumn: {
-        name: "banker",
-       referencedColumnName: "id"
-    },
-    inverseJoinColumn: {
-        name: "client",
-        referencedColumnName: "id"
-    }
-  })
-  clients: Client[]
-  
+	@ManyToMany(() => Client)
+	@JoinTable({
+		name: "Bankers_clients",
+		joinColumn: {
+			name: "banker",
+			referencedColumnName: "id",
+		},
+		inverseJoinColumn: {
+			name: "client",
+			referencedColumnName: "id",
+		},
+	})
+	clients: Client[];
 }
